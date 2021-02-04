89db5cb137883ac974e22ce24f262b52ff987b43
pboennig <pboennig@gmail.com>
2021-02-03 21:37:48 -0800

    editable database that is displayed in ride_queue

--- door2dorm/ride_queue/views.py
+++ door2dorm/ride_queue/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here.
-def index(reauest):
-    return HttpResponse("Welcome to the ride queue.")
+from .models import Ride
+
+def index(request):
+    latest_rides_list = Ride.objects.order_by('-time_requested')
+    template = loader.get_template('ride_queue/index.html')
+    context = { 
+        'latest_rides_list': latest_rides_list,
+    }
+    return render(request, 'ride_queue/index.html', context)
