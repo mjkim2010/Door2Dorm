3924bcb0e034dfdb4b38b5d3681dd92d607032ba
pboennig <pboennig@gmail.com>
2021-02-03 21:23:12 -0800

    add Ride class as model

--- door2dorm/ride_queue/models.py
+++ door2dorm/ride_queue/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Ride(models.Model):
+    sunet = models.CharField(max_length=30)
+    time_requested = models.DateTimeField('time requested')
+    num_passengers = models.IntegerField(default=1)
+
+    def __str__(self):
+        return self.sunet + " requested at {} for {} passengers".format(self.time_requested, self.num_passengers) 
