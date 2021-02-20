83eb1f519295257f17b83841ab07483346094484
mjkim2010 <mjkim9908@gmail.com>
2021-02-20 14:17:30 -0800

    Added axios code to connect to Django server

--- door2dorm/dispatcher_controller/views.py
+++ door2dorm/dispatcher_controller/views.py
@@ -7,16 +7,25 @@ from .models import Ride, Student
 def ride_queue_view(request):
     latest_rides_list = Ride.objects.order_by('time_requested')
     template = loader.get_template('ride_queue.html')
-    context = { 
+    context = {
         'latest_rides_list': latest_rides_list,
     }
     return render(request, 'ride_queue.html', context)
 
 def create_student(request):
+    latest_rides_list = Ride.objects.order_by('time_requested')
+    template = loader.get_template('ride_queue.html')
+    context = {
+        'latest_rides_list': latest_rides_list,
+    }
     student = Student.create(1213123, "testing", "mandy", "li", "jk@gmail.com", 1233123213)
     template = loader.get_template('cr_student.html')
-    context = { 
+    context = {
         'student': student,
     }
     student.save()
-    return render(request, 'cr_student.html', context)
+    if (request.method == 'GET'):
+        return render(request, 'cr_student.html', context)
+    else:
+        return render(request, 'ride_queue.html', context)
+    # return render(request, ‘cr_student.html’, context)
