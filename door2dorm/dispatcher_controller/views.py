ae5e87ebe2968af3afce741a7e5d5dd2708a9593
Shelly Deng <jsdeng@stanford.edu>
2021-03-11 01:14:28 -0800

    clean up views.py

--- door2dorm/dispatcher_controller/views.py
+++ door2dorm/dispatcher_controller/views.py
@@ -5,7 +5,6 @@ import json
 
 from .models import Ride, Student, Driver
 from .serializers import StudentSerializer, RideSerializer, DriverSerializer
-# CommentTag: MAKE_POST
 from rest_framework import viewsets, permissions
 from rest_framework.response import Response
 from rest_framework.decorators import action
